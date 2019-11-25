a2d00bc10a500d337848d27be659f0655a75d428
samsucik <sucik.samo@gmail.com>
2019-11-25 15:03:33 +0000

    Make distillation work with distiller from pytorch-transformers.

--- dbert/distill/run/distill_birnn.py
+++ dbert/distill/run/distill_birnn.py
@@ -1,7 +1,9 @@
 import os
 import sys
 import argparse
+from types import SimpleNamespace
 
+import numpy as np
 from torch.nn.utils import clip_grad_norm_
 from torch.optim import Adadelta
 from tqdm import tqdm
@@ -39,7 +41,7 @@ def evaluate_score(model, ds_iter, criterion, export_eval_labels=False):
     sr = spearmanr(preds, gts)[0]
     return pr, sr
 
-
+"""
 def evaluate(model, ds_iter, criterion, export_eval_labels=False):
     ds_iter.init_epoch()
     model.eval()
@@ -64,11 +66,61 @@ def evaluate(model, ds_iter, criterion, export_eval_labels=False):
     acc /= n
     loss /= n
     return acc, loss
+"""
 
+def evaluate(params: SimpleNamespace):
+    """
+    params contains:
+    - dataset
+    - model
+    - task_name
+    """
+    params.dataset.init_epoch()
+    params.model.eval()
+    preds, targets = None, None
+    for batch in params.dataset:
+        logits = params.model(batch.sentence)
+        labels_pred = logits.max(1)[1]
+        if preds is None:
+            preds = labels_pred.detach().cpu().numpy()
+            targets = batch.label.detach().cpu().numpy()
+        else:
+            preds = np.append(preds, labels_pred.detach().cpu().numpy(), axis=0)
+            targets = np.append(targets, batch.label.detach().cpu().numpy(), axis=0)
+    result = compute_metrics(params.task_name, preds, targets)
+    return result
+
+def extend_args(args):
+    pairing = {
+        "output_dir": "workspace",
+        "temperature": "distill_temperature",
+        "alpha_ce": "ce_lambda",
+        "alpha_mse": "distill_lambda",
+        "n_epochs": "epochs",
+        "learning_rate": "lr",
+        "max_grad_norm": "clip_grad",
+        "task_name": "dataset_name"
+    }
+    defaults = {
+        "gradient_accumulation_steps": 1,
+        "use_hard_labels": False,
+        "warmup_prop": 0,
+        "max_steps": -1,
+        "log_interval": 50,
+        "evaluate_during_training": True,
+        "checkpoint_interval": -1,
+        "n_gpu": 0 if args.device == "cpu" else 1
+    }
+    for k, v in pairing.items():
+        args.__dict__[k] = args.__dict__[v]
+    for k, v in defaults.items():
+        args.__dict__[k] = v
+    return SimpleNamespace(**args.__dict__)
 
 def main():
     args = read_args(default_config="confs/kim_cnn_sst2.json")
     set_seed(args.seed)
+    args = extend_args(args)
     print(args)
     torch.cuda.deterministic = True
     dataset_cls = find_dataset(args.dataset_name)
@@ -76,8 +128,17 @@ def main():
         batch_size=args.batch_size, device=args.device, train=args.train_file, dev=args.dev_file, test=args.test_file)
 
     args.dataset = training_iter.dataset
-    args.words_num = len(training_iter.dataset.TEXT_FIELD.vocab)
     model = mod.BiRNNModel(args).to(args.device)
+    args.dataset = None
+    distiller = Distiller(params=args,
+                          dataloader_train=training_iter,
+                          dataloader_dev=dev_iter,
+                          student=model,
+                          evaluate_fn=evaluate,
+                          student_type="LSTM")
+    distiller.train()
+    exit(0)
+
     ckpt_attrs = mod.load_checkpoint(model, args.workspace,
         best=args.load_best_checkpoint) if args.load_last_checkpoint or args.load_best_checkpoint else {}
  
@@ -158,4 +219,10 @@ def main():
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--transformers_path", type=str)
+    args, _ = parser.parse_known_args()
+    sys.path.append(os.path.abspath(args.transformers_path))
+    from examples.distillation.distiller_from_finetuned import Distiller
+    from examples.utils_glue import compute_metrics
     main()
